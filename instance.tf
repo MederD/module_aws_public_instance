45c04bc886feb18b5ead322f2ed628f8625e7d94
MederD <freenetscaper@yahoo.com>
2021-03-22 22:45:24 -0700

    test

---
+++ instance.tf
@@ -0,0 +1,33 @@
+# --------------------Public instance
+resource "aws_instance" "my_pub_instance" {
+  ami                         = var.ami_name[var.region]
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true  
+  vpc_security_group_ids      = [aws_security_group.my_pub_sg.id]
+  count                       = var.count
+  
+  tags          = {
+    Name        = "${var.prefix}-pub-instance"
+    Created_by  = var.prefix
+    Date        = timestamp()
+  }
+  
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = var.user_name
+    private_key = file(var.ssh_key_path)
+    timeout     = "4m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm",
+      "sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm",
+      "sudo yum install mysql-server -y",
+      "sudo systemctl start mysqld"
+    ]
+  }
+}  
